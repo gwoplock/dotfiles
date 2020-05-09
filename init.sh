8cc85d894bccc3cbc760725e92790c1635bba3d7
Garrett Battaglia <garrett@garrettbattaglia.info>
2020-05-09 17:44:03 -0500

    start init script

---
+++ init.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+chsh -s $(which zsh)
+ln .oh-my-zshloadrc ~/
+ln .zshaliasrc ~/
+ln .zshconfigrc ~/
+ln .zshexportrc ~/
+ln .zshpluginrc ~/
+ln .zshrc ~/
+
